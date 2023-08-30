e208a76d2a5f0da6ffbcc402d1740d4b135b1637
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-30 14:24:51 -0500

    querySelector samples

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const allLinks = document.querySelectorAll('a');
 for (let link of allLinks) {
     link.innerText = 'I am happy'
 }
+
+document.querySelector('p').innerHTML = '<b> Happiness</b>'
+
+const test = document.querySelector('span').innerText = "Disgusting";
