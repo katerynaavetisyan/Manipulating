f6282261cdcd0daaba82296cf4d13e514c1f498b
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-30 14:08:42 -0500

    change all 'a'

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const h1 = document.querySelectorAll('h1')
-console.dir(h1)
-console.log(document.querySelector('p').innerText = 'Happy Chicken');
+// const h1 = document.querySelectorAll('h1')
+// console.dir(h1)
+// console.log(document.querySelector('p').innerText = 'Happy Chicken');
+const allLinks = document.querySelectorAll('a');
+for (let link of allLinks) {
+    link.innerText = 'I am happy'
+}
