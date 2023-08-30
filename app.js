dcfb8b50c0cd9da241efd742ee2867f7407fd013
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-30 13:08:46 -0500

    practicing querySelector

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const allLinks = document.querySelectorAll('a');
-
-for (let link of allLinks) {
-    link.innerText = 'I AM A funny LINK!!!!'
-}
-
-
-// for (let link of allLinks) {
-//     link.style.color = 'rgb(0, 108, 134)';
-//     link.style.textDecorationColor = 'magenta';
-//     link.style.textDecorationStyle = 'wavy'
-// }
+const h1 = document.querySelectorAll('h1')
+console.dir(h1)
+console.log(document.querySelector('p').innerText = 'Happy Chicken');
