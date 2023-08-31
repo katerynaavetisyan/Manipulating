1e0746f29799482260dccd63f866285516175b52
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-31 11:07:02 -0500

    Magical Forest Circle Exercise

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
+const picture = document.querySelector('img');
+picture.style.width = '150px';
+picture.style.borderRadius = '50%';
 
-
-console.log(firstLink.href);
-console.log(firstLink.getAttribute('href'));
-console.log(firstLink.getAttribute('href'));
-
-console.log(firstLink.getAttribute('title'));
-console.log(firstLink.setAttribute('href', 'http://www.google.com'));
-
-
-console.log(document.querySelectorAll('input')[1]);
-
-const input = document.querySelector('input[type="text"]');
-
-console.log(input.type = 'color '); input.type = 'color';
-
-input.setAttribute('type', 'text')
+const cont = document.querySelector('#container');
+cont.style.textAlign = 'center'
